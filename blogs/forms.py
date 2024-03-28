98c610c618b98d040a7ccb1b90499d5ae06e598d
saumil8200 <saumil8200@gmail.com>
2024-03-28 13:28:26 +0530

    UI changes for all the forms and form field checks

--- blogs/forms.py
+++ blogs/forms.py
@@ -1,8 +1,29 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 from django import forms
 from .models import Blog
 
 class BlogForm(ModelForm):
     class Meta:
         model = Blog
-        fields = ['featured_image', 'title', 'description', 'category', 'tags',]
+        fields = ['category', 'title', 'description',]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Add classes and placeholders to form fields
+        self.fields['category'].widget.attrs.update({
+            'class': 'w-full bg-[#F5F5F5] p-3 rounded-2xl font-light',
+        })
+        self.fields['title'].widget.attrs.update({
+            'class': 'w-full bg-[#F5F5F5] p-3 rounded-2xl',
+            'placeholder': 'Enter title'
+        })
+        # Set the Textarea widget for the description field and adjust its attributes
+        self.fields['description'].widget = Textarea(attrs={
+            'class': 'w-full bg-[#F5F5F5] p-3 rounded-2xl',
+            'placeholder': 'Enter description',
+            'rows': 15  # Number of visible rows
+        })
+
+        # Set a default placeholder or initial value for the category field
+        self.fields['category'].empty_label = 'Select category'
--- users/forms.py
+++ users/forms.py
@@ -9,3 +9,31 @@ class CustomUserCreationForm(UserCreationForm):
         labels = {
              'first_name': 'Name',
             }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Add classes and placeholders to form fields
+        self.fields['first_name'].widget.attrs.update({
+            'class': 'w-full bg-[#F5F5F5] p-3 rounded-2xl',
+            'placeholder': 'Enter name'
+        })
+
+        self.fields['email'].widget.attrs.update({
+            'class': 'w-full bg-[#F5F5F5] p-3 rounded-2xl',
+            'placeholder': 'Enter email'
+        })
+
+        self.fields['username'].widget.attrs.update({
+            'class': 'w-full bg-[#F5F5F5] p-3 rounded-2xl',
+            'placeholder': 'Enter username'
+        })
+
+        self.fields['password1'].widget.attrs.update({
+            'class': 'w-full bg-[#F5F5F5] p-3 rounded-2xl',
+            'placeholder': 'Enter Password'
+        })
+
+        self.fields['password2'].widget.attrs.update({
+            'class': 'w-full bg-[#F5F5F5] p-3 rounded-2xl',
+            'placeholder': 'Confirm password'
+        })
