0da394da5ccb832ba7d931a1b25c38c26e3d3c12
saumil8200 <saumil8200@gmail.com>
2024-03-28 14:15:43 +0530

    minor UI elemt changes mobile responsive navbar and etc

--- myblog/urls.py
+++ myblog/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogs.urls')),
-    path('profiles/', include('users.urls'))
+    path('authors/', include('users.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
